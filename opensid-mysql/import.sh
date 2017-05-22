b4e8ed0e615069e367f37150ae0499d878ad0554
Samsul Ma'arif <hay@samsul.web.id>
2017-05-23 00:35:12 +0700

    fix GRANT ALL command
    Signed-off-by: Samsul Ma'arif <hay@samsul.web.id>

--- opensid-mysql/import.sh
+++ opensid-mysql/import.sh
@@ -3,5 +3,5 @@
 sleep 5
 mysql -u root -e "CREATE USER opensid;" 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS opensid CHARACTER SET utf8 COLLATE utf8_general_ci;" 
-mysql -u root -e "GRANT ALL ON 'opensid'.* to 'opensid'@'%' IDENTIFIED BY 'opensid';" 
+mysql -u root -e "GRANT ALL ON opensid.* to 'opensid'@'%' IDENTIFIED BY 'opensid';" 
 mysql -u root opensid < /root/contoh.sql
